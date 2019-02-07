69fa68f4f2073626789f91036b25d56c3364927d
Matthieu-Furvent <ze.karel@free.fr>
2019-02-07 16:53:47 +0100

    Add pointer color, some modif on tchat. WIP.

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-//Exemple de chat
-
+"use strict";
 
 //On importe les dépendances
 let app = require('express')();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
+let users = new Map();
+
 //On envoie le fichier html de base à notre serveur
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/lodash', function (req, res) {
+    res.sendFile(__dirname + '/lodash.min.js');
 });
 
 //On lance notre serveur
@@ -23,10 +28,24 @@ io.on('connection', function (socket) {
     console.log("un utilisateur s'est connecté");
     //Quand on reçoit un message de l'utilisateur on fait quelque chose
     socket.on("msg", function (message) {
+        let pseudo = users.get(socket.id).pseudo;
         //On veut renvoyer le message a tous les utilisateurs connectés
         //On renvoie le message a tout le monde y compris à celui qui l'a envoyé, pour l'envoyer uniquement à certaines
         // personnes on peut utiliser plutot broadcast
-        io.emit("msg", message);
+        // And we send pseudo
+        io.emit("msg", { mess: message, psd: pseudo });
+    });
+    socket.on("getPseudo", function (pseudo) {
+        console.log("User: " + socket.id + " chosen an alias: " + pseudo);
+        // On reçoit le pseudo de l'utilisateur et on le stocke dans la map.
+        users.set(socket.id, { pseudo: pseudo });
+    });
+    socket.on('mousemove', function (data) {
+        data.id = socket.id;
+        socket.broadcast.emit('mousemove', data);
+    });
+    socket.on('disconnect', function() {
+        socket.broadcast.emit('userdisconnected', {id: socket.id})
     });
 });
 
